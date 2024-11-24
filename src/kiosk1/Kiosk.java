74b424d30586eefecb044b5af572132378161f89
joacho <joacho1205@gmail.com>
2024-11-24 15:03:22 +0900

    Lv3 Kiosk클래스 생성

---
+++ src/kiosk1/Kiosk.java
@@ -0,0 +1,11 @@
+package kiosk1;
+
+/**
+ *  키오스크 프로그램의 메뉴를 관리하고 사용자 입력을 처리하는 클래스
+ *  main함수에서 관리하던 입력과 반복문 로직은 이제 start 함수를 만들어 관리한다.
+ *  MenuItem을 관리하는 리스트가 필드로 존재한다.
+ *  List<MenuItem>>menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당한다.
+ *  -> Kiosk 객체를 생성하고 사용하는 main 함에서 객체를 생성할 때 값을 넘겨준다.
+ */
+public class Kiosk {
+}
